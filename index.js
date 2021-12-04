5869ee80eb25b014851c38c85defc681ef5c4b6f
Vikalp Malhotra <kimboho@gmail.com>
2021-12-03 19:09:59 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.querySelector('main#main').remove()
+const newHeader = document.createElement('h1')
+newHeader.setAttribute('id', "victory")
+newHeader.textContent = 'Vikalp is the champion';
+
+document.body.append(newHeader)
+
